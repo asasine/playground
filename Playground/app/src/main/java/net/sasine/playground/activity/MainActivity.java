269694bb5a3a766216e1b0d5c1c92551ed4e27a3
Adam <adam.sasine@gmail.com>
2017-01-25 14:19:42 -0500

    Basic CardView list using RecyclerView

---
+++ Playground/app/src/main/java/net/sasine/playground/activity/MainActivity.java
@@ -0,0 +1,49 @@
+package net.sasine.playground.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import net.sasine.playground.R;
+import net.sasine.playground.fragment.ItemFragment;
+import net.sasine.playground.fragment.MyItemRecyclerViewAdapter;
+import net.sasine.playground.fragment.dummy.DummyContent;
+
+public class MainActivity extends Activity implements ItemFragment.OnListFragmentInteractionListener {
+
+    private final String TAG = "MainActivity";
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyItemRecyclerViewAdapter(DummyContent.ITEMS, null);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
+
+    // Called by activity_main.xml button:onClick
+    public void addDummyItem(View view) {
+        // add DummyItem to DummyContent
+        Log.i(TAG, "Creating DummyItem");
+    }
+}
---
+++ Playground/app/src/main/java/net/sasine/playground/fragment/MyItemRecyclerViewAdapter.java
@@ -0,0 +1,85 @@
+package net.sasine.playground.fragment;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import net.sasine.playground.R;
+import net.sasine.playground.fragment.ItemFragment.OnListFragmentInteractionListener;
+import net.sasine.playground.fragment.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder> {
+
+    private final List<DummyItem> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyItemRecyclerViewAdapter(List<DummyItem> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    // Return the size of your dataset (involed by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public DummyItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.id);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
